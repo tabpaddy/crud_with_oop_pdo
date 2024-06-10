92c5c34b810dece65263a1495519f46aad9fd90c
smallboyintech <111036308+tabpaddy@users.noreply.github.com>
2024-06-11 00:44:20 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const addForm = document.getElementById("add-user-form");
+const showAlert = document.getElementById("showAlert");
+const addModal = new bootstrap.Modal(document.getElementById("addNewUserModal"));
+// Add new User AJAX request
+addForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(addForm);
+    formData.append("add", 1);
+
+    if(addForm.checkValidity() === false){
+        e.preventDefault();
+        e.stopPropagation();
+        addForm.classList.add("was-validated");
+        return false;
+    } else {
+        document.getElementById('add-user-btn').value = 'Please Wait...';
+
+        const data = await fetch('./controller/action.php', {
+            method: 'POST',
+            body: formData,
+        });
+        const response = await data.text();
+        showAlert.innerHTML = response;
+        document.getElementById('add-user-btn').value = 'Add User';
+        // reset the form
+        addForm.reset();
+        addForm.classList.remove("was-validated");
+        addModal.hide();
+    }
+});
